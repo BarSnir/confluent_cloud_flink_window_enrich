1ffff7809ace1dc1fafbd430d1b5d12e500a466c
BarSnir <bar1991snir@gmail.com>
2025-09-10 23:18:36 +0000

    remove redundat sqls

--- sqls/temporal.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  v.`KM`
-FROM `Orders` AS o
-JOIN `vehicles_extract` FOR SYSTEM_TIME AS OF o.`$rowtime` AS v
-  ON o.OrderId = v.OrderId;
--- sqls/window.sql
+++
@@ -1,4 +0,0 @@
-SELECT window_start, window_end, OrderId, SUM(Price) as `sum`
-  FROM TABLE(
-    TUMBLE(TABLE `Orders`, DESCRIPTOR($rowtime), INTERVAL '5' MINUTES))
-  GROUP BY window_start, window_end, OrderId;
